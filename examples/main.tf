ef30235e0982c2c23381cc02ae4369fe2eba08ab
borgified <borgified@gmail.com>
2019-11-08 02:29:36 -0800

    update example

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,6 @@
 module "dashboard" {
   source  = "borgified/dashboard/datadog"
-  version = "0.1.8"
+  version = "0.1.10"
   prefix      = "app.dev.memstats"
   api_key     = "your datadog api key"
   app_key     = "your datadog app key"
