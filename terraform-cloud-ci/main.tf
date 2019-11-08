e0fe0de5b42e91a79432a1b327e69528ac175fbb
borgified <borgified@gmail.com>
2019-11-08 02:13:41 -0800

    move jq logic into list_metrics.sh

--- terraform-cloud-ci/main.tf
+++ terraform-cloud-ci/main.tf
@@ -1,17 +1,6 @@
-resource "null_resource" "get_jq" {
-  provisioner "local-exec" {
-    command = "curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq && chmod u+x jq"
-  }
-  triggers = {
-    redownload_every_apply = "${timestamp()}"
-  }
-}
-
 module "dashboard" {
-  source  = "borgified/dashboard/datadog"
-  version = "0.1.9"
+  source  = "../"
   prefix      = var.prefix
   api_key     = var.api_key
   app_key     = var.app_key
-  dependency  = [ null_resource.get_jq.id ]
 }
