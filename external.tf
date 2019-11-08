7c12cd44b366b7fa92c43dbfc07b7d58167cc3f1
borgified <borgified@gmail.com>
2019-11-07 23:06:34 -0800

    install jq for terraform cloud

--- external.tf
+++ external.tf
@@ -1,6 +1,12 @@
-data "external" "list_metrics" {
-  program = ["bash", "${path.module}/list_metrics.sh"]
+resource "null_resource" "get_jq" {
+  provisioner "local-exec" {
+    command = "curl -sL https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -o jq && chmod u+x jq"
+  }
+}
 
+data "external" "list_metrics" {
+  depends_on = [ null_resource.get_jq ]
+  program = ["bash", "${path.module}/list_metrics.sh"]
   query = {
     api_key = var.api_key
     app_key = var.app_key
