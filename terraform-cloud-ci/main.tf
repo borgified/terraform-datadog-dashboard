27b063b0e63438748284cfe7c369cc208e895e5b
borgified <borgified@gmail.com>
2019-11-21 17:39:28 -0800

    Update main.tf

--- terraform-cloud-ci/main.tf
+++ terraform-cloud-ci/main.tf
@@ -3,6 +3,6 @@ module "dashboard" {
   prefix      = var.prefix
   api_key     = var.api_key
   app_key     = var.app_key
-  space_aggregation = "avg"
-  scope = "*"
+  space_aggregation = var.space_aggregation
+  scope = var.scope
 }
